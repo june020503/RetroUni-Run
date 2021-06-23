0074f99f305a689e697e9c8429ee3a3b8ea4f834
june020503 <june020503@gmail.com>
2021-06-23 18:28:43 +0900

    Background loop and UI(Text)

---
+++ 2d platformer(Retr0)/Assets/Scripts/BackgroundLoop.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//왼쪽 끝으로 이동한 배경을 오른쪽 끝으로 재배치하는 스크립트
+public class BackgroundLoop : MonoBehaviour
+{
+    private float width;
+
+    private void Awake()
+    {
+        BoxCollider2D backgroundCollider = GetComponent<BoxCollider2D>();
+        width = backgroundCollider.size.x;    
+    }
+
+    private void Update()
+    {
+        if(transform.position.x <= -width)
+        {
+            Reposition();
+        }
+    }
+
+    private void Reposition()
+    {
+        Vector2 offset = new Vector2(width * 2f, 0);
+        transform.position = (Vector2)transform.position + offset;
+    }
+}
